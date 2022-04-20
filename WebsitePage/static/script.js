9bd789bcf5a005d618a4f8b655341f5f01c4d180
syazzi <inazuma78910@gmail.com>
2022-04-20 20:39:25 +0800

    update
    add-drop-modules

---
+++ WebsitePage/static/script.js
@@ -0,0 +1,23 @@
+function disable_select(){
+    document.getElementById('first-priority').disabled = true
+}
+
+function disable_select_2(){
+    document.getElementById('second-priority').disabled = true
+}
+
+function disable_select_3(){
+    document.getElementById('third-priority').disabled = true
+}
+
+function disable_select_4(){
+    document.getElementById('fourth-priority').disabled = true
+}
+
+function disable_select_5(){
+    document.getElementById('fifth-priority').disabled = true
+}
+
+function enable_select(){
+    document.getElementById('first-priority').disabled = false
+}
